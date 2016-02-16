82e5dd308a9203f50a01aa85ec356cd8806f8734
Klockner <ngsoulk@gmail.com>
2016-02-15 23:37:21 -0200

    JPA works
    JPA funcionando e cadastrando usuário no banco

--- gabriel/src/main/java/br/com/klockner/gabriel/controller/usuario/UsuarioController.java
+++ gabriel/src/main/java/br/com/klockner/gabriel/controller/usuario/UsuarioController.java
@@ -8,26 +8,18 @@ import javax.faces.context.FacesContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 import br.com.klockner.gabriel.model.usuario.Usuario;
 import br.com.klockner.gabriel.service.usuario.UsuarioService;
 
 /**
-* A @SessionScoped: The session scope persists from the time that a session is established
-* until session termination.
-* A @RequestScoped: The request scope is short-lived. It starts when an HTTP request 
-* is submitted and ends after the response is sent back to the client.
-* An @ApplicationScoped: The application scope persists for the entire duration of the
-* web application. That scope is shared among all requests and all sessions.
-* A @ViewScoped: View scope was added in JSF 2.0. A bean in view scope persists while the 
-* same JSF page is redisplayed. (The JSF specification uses the term view for a JSF page.) As soon as the user navigates to a different page, the bean goes out of scope.
 * 
 * @author Gabriel Klockner
 */
 
-@Component
-@Scope("session")
+@Controller
+@Scope("request")
 public class UsuarioController {
 	private Usuario usuario = new Usuario();
 	
@@ -37,8 +29,8 @@ public class UsuarioController {
 	public void salvar() {
 		usuarioService.salvar(usuario);
 		//TODO verificar se usuario ja existe no banco e mandar mensagem de erro
-//		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, 
-//						"Cadastro realizado com sucesso.", null));
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, 
+						"Cadastro realizado com sucesso.", null));
 	}
 	
 	public UsuarioService getUsuarioService() {
